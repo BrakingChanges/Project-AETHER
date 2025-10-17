163de3748022f2acd0e7857d1e33a99269e633f8
Aiden Omondi <helpertech83@gmail.com>
2025-10-17 11:44:55 +0300

    Level 1 done

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,25 +1,28 @@
 extends CharacterBody2D
+class_name PlayerController
 
-@export var speed: float = 500.0
-@export var jump_velocity: float = -500.0
-@onready var sprite: Sprite2D = $Sprite
+@export var speed = 10.0
+@export var jump_power = 10.0
 
-@onready var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var speed_multiplier = 30.0
+var jump_multiplier = -30.0
+var direction = 0
 
-func _physics_process(delta):
-	# Apply gravity
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity += get_gravity() * delta
 
-	# Move left/right
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
-		velocity.x = direction * speed
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed * speed_multiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed * delta)
-
-	# Jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
--- scripts/base_level.gd
+++ scripts/base_level.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-@onready var player_spawn = $PlayerSpawn
-@onready var goal = $Goal
-
 func _ready():
 	modulate = Color(0.8, 0.9, 1.0) # slight glow tint
 
 
 
-func _on_goal_body_entered(_body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/Main Menu.tscn")
+func _on_goal_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://scenes/Main Menu.tscn")
